4e46b98276c09dacf906cc1637384a4b2a79a8f3
MinhNgocNgo <minhngoc.ngo1999@gmail.com>
2024-09-12 15:33:49 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -13,7 +13,7 @@ def hello_world():
                                                                                                                                        
 @app.route("/contact/")
 def MaPremiereAPI():
-    “return render\_template("contact.html")”
+    “return render_template('contact.html')
 
 @app.route('/tawarano/')
 def meteo():
